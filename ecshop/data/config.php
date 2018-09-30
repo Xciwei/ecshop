75ed1472cd529465933dc47075a9d52a032b5658
Xciwei <619506288@qq.com>
2018-09-30 14:57:00 +0800

    demo
    d

---
+++ ecshop/data/config.php
@@ -0,0 +1,37 @@
+<?php
+// database host
+$db_host   = "172.16.11.227:3306";
+
+// database name
+$db_name   = "ecshop";
+
+// database username
+$db_user   = "baison";
+
+// database password
+$db_pass   = "baison123";
+
+// table prefix
+$prefix    = "ecs_";
+
+$timezone    = "Europe/Berlin";
+
+$cookie_path    = "/";
+
+$cookie_domain    = "";
+
+$session = "1440";
+
+define('EC_CHARSET','utf-8');
+
+define('ADMIN_PATH','admin');
+
+define('AUTH_KEY', 'this is a key');
+
+define('OLD_AUTH_KEY', '');
+
+define('API_TIME', '');
+
+define('STORE_KEY','7aa12dda21de10ba9d4d0b8c17ee8601');
+
+?>
---
+++ ecshop/temp/query_caches/sqlcache_config_file_ff644f2b3bde71ecf13bbb60447be066.php
@@ -0,0 +1,6 @@
+<?php
+$this->mysql_config_cache_file_time = 1538290211;
+$this->timeline = 20;
+$this->timezone = 0;
+$this->platform = 'OTHER';
+?>
